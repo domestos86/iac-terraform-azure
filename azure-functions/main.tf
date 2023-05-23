226915bded6c2452db27380be4cb46c02f26d64a
domestos86 <105596429+domestos86@users.noreply.github.com>
2023-05-23 20:34:23 +0200

    Update main.tf

--- azure-functions/main.tf
+++ azure-functions/main.tf
@@ -1,6 +1,6 @@
 resource "azurerm_resource_group" "example" {
   name     = var.resource_group_name
-  location = var.location
+  location = var.resource_group_location
 }
 
 resource "azurerm_storage_account" "example" {
