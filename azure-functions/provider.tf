9908862bf5256ceeba5dfaa959d7c3a2d270f4da
domestos86 <105596429+domestos86@users.noreply.github.com>
2023-05-23 20:46:26 +0200

    Update provider.tf

--- azure-functions/provider.tf
+++ azure-functions/provider.tf
@@ -1,16 +1,3 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~>3.57.0"
-    }
-  }
-}
-
 provider "azurerm" {
-  features {}
-  subscription_id = "<ID subskrypcji Azure>"
-  client_id       = "<ID klienta Azure AD>"
-  client_secret   = "<Hasło klienta Azure AD>"
-  tenant_id       = "<ID tenantu Azure AD>"
+  version = "<wersja Azure>"
 }
