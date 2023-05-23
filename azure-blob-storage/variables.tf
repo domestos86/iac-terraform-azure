423a608cd358444ad7d24b98097ba02d0ebf8024
domestos86 <105596429+domestos86@users.noreply.github.com>
2023-05-23 21:07:29 +0200

    Update variables.tf

--- azure-blob-storage/variables.tf
+++ azure-blob-storage/variables.tf
@@ -1,1 +1,24 @@
+variable "resource_group_name" {
+  description = "Nazwa grupy zasobów"
+  type        = string
+}
 
+variable "resource_group_location" {
+  description = "Lokalizacja grupy zasobów"
+  type        = string
+}
+
+variable "storage_account_name" {
+  description = "Nazwa konta Azure Blob Storage"
+  type        = string
+}
+
+variable "storage_container_name" {
+  description = "Nazwa kontenera Azure Blob Storage"
+  type        = string
+}
+
+variable "storage_blob_name" {
+  description = "Nazwa bloba"
+  type        = string
+}
